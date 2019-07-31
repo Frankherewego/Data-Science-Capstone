a7984abaa34731681cacf9fb4163038d2d177fa5
FrankHe <frankyihe@hotmail.com>
2019-07-31 21:48:52 +1000

    course9 capstone

--- python_code.py
+++ python_code.py
@@ -1,11 +1,14 @@
 
 # coding: utf-8
 
-# In[ ]:
+# This notebook will be mainly used for the capstone project 
+# 
+# 
+
+# In[1]:
 
 
-#This notebook will be mainly used for the capstone project
-import pandas as pd
-import numpy as np
-print ("Hello Capstone Project Course!")
+import pandas as pd 
+import numpy as np 
+print ("Hello Capstone Project Course!") 
 
